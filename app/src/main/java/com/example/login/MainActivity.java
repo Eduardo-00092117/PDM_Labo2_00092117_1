578b5626424a70f7852fa923aed6efbc1d899c55
Eduardo López <00092117@uca.edu.sv>
2019-03-15 14:24:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txt1, txt2;
+    private Button btn;
+    private TextView mostrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt1 = findViewById(R.id.txt1);
+        txt2 = findViewById(R.id.txt2);
+        btn = findViewById(R.id.btn1);
+        mostrar = findViewById(R.id.mostrar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrar.setText(txt1.getText().toString());
+            }
+        });
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                mostrar.setText(txt2.getText().toString());
+                return false;
+            }
+        });
+    }
+}
